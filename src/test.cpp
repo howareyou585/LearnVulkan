417341ac365ea8dbdfb0a4ef930dd5da5dcbc3ed
gonggu <howareyou_mm@163.com>
2025-09-11 22:21:11 +0800

    搭建vulkan开发环境

---
+++ src/test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#define GLFW_INCLUDE_VULKAN
+#include <glfw/glfw3.h>
+using namespace std;
+const int width = 1280;
+const int height = 1080;
+const std::string title = "triangle";
+int main()
+{
+
+    if(glfwInit()==GLFW_FALSE)
+    {
+        std::cout<<"Failed to init glfw!" << std::endl;
+        return -1;
+    }
+    glfwWindowHint(GLFW_CLIENT_API,GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE,GLFW_FALSE);
+    GLFWwindow* window = glfwCreateWindow(width,height,title.c_str(),nullptr,nullptr);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
--- test.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#define GLFW_INCLUDE_VULKAN
-#include <glfw/glfw3.h>
-int main()
-{
-    return 0;
-}
